a8b36be66d55b2f54ab45cc949e7aa60e9b067c3
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-25 00:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn = document.querySelectorAll(".btn")
+const stop = document.querySelector(".stop")
+for(let i=0; i<=btn.length-1; i++){
+	btn[i].addEventListener("click", (e)=>{
+		audio[i].play()
+	stop.addEventListener("click", ()=>{
+		audio[i].pause()
+	})
+	})
+}
